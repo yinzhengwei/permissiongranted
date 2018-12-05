3b9d6e2871e1b1d00f8f567816b4892287f10b92
yinzhengwei <yinzhengwei@hexindai.com>
2018-12-05 17:39:43 +0800

    review

--- app/src/main/java/com/yzw/permissiongranted/PermissionFetchUI.kt
+++ app/src/main/java/com/yzw/permissiongranted/PermissionFetchUI.kt
@@ -64,7 +64,11 @@ class PermissionFetchUI : Activity() {
             if (isAllWaysRequest) {
                 //勾选了对话框中”Don’t ask again”的选项, 返回false,则走自定义弹窗；否则不走自定义弹窗，去请求系统弹窗
                 for ( deniedPermission in permissions) {
-                    val flag = shouldShowRequestPermissionRationale(deniedPermission);
+                    val flag = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        shouldShowRequestPermissionRationale(deniedPermission)
+                    } else {
+                       true
+                    }
                     if (!flag) {
                         //拒绝授权
                         showPermissions(this, permissionName, REQUESTCODE) {
